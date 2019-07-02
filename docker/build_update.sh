46c6f8408da90037412b7187b9b520c6d5131e2e
SpootDev <spootdev@gmail.com>
2019-07-02 10:37:11 -0500

    move from main deployment repo

--- docker/build_update.sh
+++ docker/build_update.sh
@@ -3,11 +3,20 @@ git pull
 cd ../clair
 docker build -t mediakraken/mkclair .
 
+cd ../elk
+docker build -t mediakraken/mkelk .
+
 cd ../jenkins
 docker build -t mediakraken/mkjenkins .
+
+cd ../pgadmin4
+docker build -t mediakraken/mkpgadmin .
 
 cd ../sonarqube
 docker build -t mediakraken/mksonarqube .
 
 cd ../testssl
 docker build -t mediakraken/mktestssl .
+
+cd ../wireshark
+docker build -t mediakraken/mkwireshark .
--- docker/images_push_to_hub.sh
+++ docker/images_push_to_hub.sh
@@ -1,4 +1,7 @@
 docker push mediakraken/mkclair:latest
+docker push mediakraken/mkelk:latest
 docker push mediakraken/mkjenkins:latest
+docker push mediakraken/mkpgadmin:latest
 docker push mediakraken/mksonarqube:latest
 docker push mediakraken/mktestssl:latest
+docker push mediakraken/mkwireshark:latest
