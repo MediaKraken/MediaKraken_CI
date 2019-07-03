1521cd6348b5ab3ec744ee0f9380a0598a8d14d1
SpootDev <spootdev@gmail.com>
2019-07-02 21:00:47 -0500

    pid waits

--- source/build_dev_images.py
+++ source/build_dev_images.py
@@ -29,17 +29,19 @@ CWD_HOME_DIRECTORY = '/root'
 if not os.path.exists(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment')):
     # backup to main dir with checkouts
     os.chdir(CWD_HOME_DIRECTORY)
-    subprocess.Popen(
+    pid_proc = subprocess.Popen(
         shlex.split('git clone -b dev https://github.com/MediaKraken/MediaKraken_Deployment'))
+    pid_proc.wait()
     os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment/docker/alpine'))
 else:
     # cd to MediaKraken_Deployment dir
     os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment/docker/alpine'))
     # pull down latest code
-    subprocess.Popen(['git', 'pull'])
+    pid_proc = subprocess.Popen(['git', 'pull'])
+    pid_proc.wait()
 
 # sync the latest code into the image locations for build
-os.subprocess.Popen([os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_CI', 'source_sync.sh')])
+subprocess.Popen([os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_CI', 'source_sync.sh')])
 
 for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
                      common_docker_images.STAGE_TWO_IMAGES,
@@ -48,8 +50,10 @@ for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
         # do the actual build process for docker image
         os.chdir(os.path.join(CWD_HOME_DIRECTORY,
                               'MediaKraken_Deployment/docker/alpine/%s' % docker_images))
-        subprocess.Popen(shlex.split('docker build -t mediakraken/%s:dev'
-                                     ' --build-arg ALPMIRROR=%s --build-arg PIPMIRROR=%s .') %
-                         (build_stages[docker_images][0],
-                          common_docker_images.ALPINE_MIRROR,
-                          common_docker_images.PYPI_MIRROR))
+        pid_proc = subprocess.Popen(shlex.split('docker build -t mediakraken/%s:dev'
+                                                ' --build-arg ALPMIRROR=%s'
+                                                ' --build-arg PIPMIRROR=%s .') %
+                                    (build_stages[docker_images][0],
+                                     common_docker_images.ALPINE_MIRROR,
+                                     common_docker_images.PYPI_MIRROR))
+        pid_proc.wait()
