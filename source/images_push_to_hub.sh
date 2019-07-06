26a8a745be0ec36cce9d26da177ddb0d4b28b9b2
SpootDev <spootdev@gmail.com>
2019-07-06 18:41:50 -0500

    retire

--- source/images_push_to_hub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker push mediakraken/mkelk:latest
-docker push mediakraken/mkjenkins:latest
-docker push mediakraken/mkpgadmin:latest
-docker push mediakraken/mkregistry:latest
-docker push mediakraken/mksonarqube:latest
-docker push mediakraken/mktestssl:latest
-docker push mediakraken/mkwireshark:latest
