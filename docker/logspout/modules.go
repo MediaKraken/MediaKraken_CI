2d275c638f9de7f5b10041b3917110f25b8c4c87
SpootDev <spootdev@gmail.com>
2019-08-06 21:37:36 -0500

    remove the plugin

--- docker/logspout/modules.go
+++ docker/logspout/modules.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    _ "github.com/looplab/logspout-logstash"
 	_ "github.com/gliderlabs/logspout/healthcheck"
 	_ "github.com/gliderlabs/logspout/adapters/raw"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
