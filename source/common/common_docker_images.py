b2a8affe9f4fe60bb68ee0a616a4e1df04506c90
SpootDev <spootdev@gmail.com>
2020-02-25 12:39:39 -0600

    async debian

--- source/common/common_docker_images.py
+++ source/common/common_docker_images.py
@@ -131,6 +131,7 @@ STAGE_COMPOSE_IMAGES = {'ComposeMediaKrakenBarman': ('mkbarman', 'debian:jessie'
                         'ComposeMediaKrakenTraefik': ('mktraefik', 'alpine:3.11', 'alpine'),
                         'ComposeMediaKrakenTVHeadend': ('mktvheadend', 'lsiobase/alpine:3.10', 'alpine'),
                         'ComposeMediaKrakenWebAsync': ('mkwebappasync', 'mkbase311_3py3', 'alpine'),
+                        'ComposeMediaKrakenWebAsync': ('mkwebappasyncdeb', 'mkbasedeb10_2py3', 'debian'),
                         'ComposeMediaKrakenWebServer': ('mkwebapp', 'mkbase311_3py3', 'alpine'),
                         'ComposeMediaKrakenWebServer': ('mkwebappdeb', 'mkbasedeb10_2py3', 'debian'),
                         }
