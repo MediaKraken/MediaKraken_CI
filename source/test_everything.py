e561d23955d0598ed87ee4aca42370e6bc821c9d
spootdev <spootdev@gmail.com>
2020-04-12 18:04:38 -0500

    remove other scanner

--- source/test_everything.py
+++ source/test_everything.py
@@ -293,47 +293,48 @@ for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
                                                 smtp_server=os.environ['MAILSERVER'],
                                                 smtp_port=os.environ['MAILPORT'])
 
-# change dir for anchore scanner
-os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_CI', 'docker/anchore/'))
-for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
-                     common_docker_images.STAGE_TWO_IMAGES,
-                     common_docker_images.STAGE_COMPOSE_IMAGES):
-    for docker_images in build_stages:
-        # Run Clair on each image
-        try:
-            pid_proc = subprocess.Popen(
-                shlex.split('docker-compose exec engine-api anchore-cli image vuln'
-                            ' %s/mediakraken/%s:dev all' %
-                            (common_docker_images.DOCKER_REPOSITORY,
-                             build_stages[docker_images][0])),
-                stdout=subprocess.PIPE, shell=False)
-        except subprocess.CalledProcessError as e:
-            print(e.output, flush=True)
-            sys.exit()
-        email_body = ''
-        try:
-            while True:
-                line = pid_proc.stdout.readline()
-                if not line:
-                    break
-                email_body += line.decode("utf-8")
-                print(line.rstrip(), flush=True)
-            pid_proc.wait()
-        except:
-            pass
-        common_network_email.com_net_send_email(os.environ['MAILUSER'], os.environ['MAILPASS'],
-                                                os.environ['MAILUSER'],
-                                                'Anchore image scan: '
-                                                + build_stages[docker_images][0],
-                                                email_body,
-                                                smtp_server=os.environ['MAILSERVER'],
-                                                smtp_port=os.environ['MAILPORT'])
+# TODO the exec below isn't right, might need to talk to seperate server
+# # change dir for anchore scanner
+# os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_CI', 'docker/anchore/'))
+# for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
+#                      common_docker_images.STAGE_TWO_IMAGES,
+#                      common_docker_images.STAGE_COMPOSE_IMAGES):
+#     for docker_images in build_stages:
+#         # Run Clair on each image
+#         try:
+#             pid_proc = subprocess.Popen(
+#                 shlex.split('docker-compose exec engine-api anchore-cli image vuln'
+#                             ' %s/mediakraken/%s:dev all' %
+#                             (common_docker_images.DOCKER_REPOSITORY,
+#                              build_stages[docker_images][0])),
+#                 stdout=subprocess.PIPE, shell=False)
+#         except subprocess.CalledProcessError as e:
+#             print(e.output, flush=True)
+#             sys.exit()
+#         email_body = ''
+#         try:
+#             while True:
+#                 line = pid_proc.stdout.readline()
+#                 if not line:
+#                     break
+#                 email_body += line.decode("utf-8")
+#                 print(line.rstrip(), flush=True)
+#             pid_proc.wait()
+#         except:
+#             pass
+#         common_network_email.com_net_send_email(os.environ['MAILUSER'], os.environ['MAILPASS'],
+#                                                 os.environ['MAILUSER'],
+#                                                 'Anchore image scan: '
+#                                                 + build_stages[docker_images][0],
+#                                                 email_body,
+#                                                 smtp_server=os.environ['MAILSERVER'],
+#                                                 smtp_port=os.environ['MAILPORT'])
 
 #####################################
 # start up the application so can see running images for several tools
 #####################################
-os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment', 'run_configs/compose_dev/'))
-pid_proc = subprocess.Popen(shlex.split('mediakraken_start.sh'),
+os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment', 'run_configs/compose_dev'))
+pid_proc = subprocess.Popen(shlex.split('./mediakraken_start.sh'),
                             stdout=subprocess.PIPE, shell=False)
 while True:
     line = pid_proc.stdout.readline()
@@ -362,11 +363,11 @@ common_network_email.com_net_send_email(os.environ['MAILUSER'], os.environ['MAIL
                                         smtp_server=os.environ['MAILSERVER'],
                                         smtp_port=os.environ['MAILPORT'])
 
-# run under started app as need the db connection
-# TODO that won't work.....as it's a docker db
-pytest_pid = subprocess.Popen(shlex.split(
-    'python3 -m pytest --capture=no testing/test_common/test_common_network_youtube.py'))
-pytest_pid.wait()
+# # run under started app as need the db connection
+# # TODO that won't work.....as it's a docker db
+# pytest_pid = subprocess.Popen(shlex.split(
+#     'python3 -m pytest --capture=no testing/test_common/test_common_network_youtube.py'))
+# pytest_pid.wait()
 
 #####################################
 # run application web test/etc
@@ -451,8 +452,8 @@ common_network_email.com_net_send_email(os.environ['MAILUSER'], os.environ['MAIL
 #####################################
 # stop the application
 #####################################
-os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment', 'run_configs/compose_dev/'))
-pid_proc = subprocess.Popen(shlex.split('mediakraken_stop.sh'),
+os.chdir(os.path.join(CWD_HOME_DIRECTORY, 'MediaKraken_Deployment', 'run_configs/compose_dev'))
+pid_proc = subprocess.Popen(shlex.split('./mediakraken_stop.sh'),
                             stdout=subprocess.PIPE, shell=False)
 pid_proc.wait()
 # this sleep is here so that everything has time to fully stop like pika
