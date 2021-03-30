fb5257e7564d0a6930adfb10f65c8e9feac8a2ea
SpootDev <spootdev@gmail.com>
2021-03-29 20:14:21 -0500

    full path?

--- source/validate_docker.py
+++ source/validate_docker.py
@@ -51,7 +51,8 @@ for build_stages in (common_docker_images.STAGE_ONE_IMAGES,
         # Run hadolint on each image
         try:
             pid_proc = subprocess.Popen(
-                shlex.split('docker run --rm -i hadolint/hadolint < Dockerfile'),
+                shlex.split('docker run --rm -i hadolint/hadolint < %s'
+                            % os.path.join(os.getcwd(), 'Dockerfile')),
                 stdout=subprocess.PIPE, shell=False)
         except subprocess.CalledProcessError as e:
             print(e.output, flush=True)
