1f2bd59a3336ca851aa425201db7346ce424d4b7
SpootDev <spootdev@gmail.com>
2021-06-19 13:43:11 -0500

    not using opensssl build for this

--- source/common/common_docker_images.py
+++ source/common/common_docker_images.py
@@ -67,7 +67,7 @@ STAGE_TWO_IMAGES = {
 docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl cargo build --release
 '''
 STAGE_RUST_OPENSSL_IMAGES = {
-    'ComposeMediaKrakenBroadcastRust': ('mkbroadcastrust', 'alpine:3.13.5', 'alpine'),
+
 }
 
 # these are the final images
