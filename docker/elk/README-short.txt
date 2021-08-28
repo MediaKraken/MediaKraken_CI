f094f400ea1567b6105b830aa8b6b7630a2a690b
spootdev <spootdev@gmail.com>
2021-08-28 16:22:16 -0500

    move to mk

--- docker/elk/README-short.txt
+++
@@ -1,1 +0,0 @@
-Collect, search and visualise log data with ELK (Elasticsearch 7.11.2, Logstash 7.11.2, Kibana 7.11.2).
--- source/joxit_registry_ui_start.sh
+++
@@ -1,3 +0,0 @@
-docker run -d -p 8000:80 -e REGISTRY_URL=th-registry-1.beaverbay.local:5000 -e DELETE_IMAGES=true th-registry-1.beaverbay.local:5000/mediakraken/mkjoxitui:dev
-
-docker run -d   -p 5000:5000   --restart=always   --name mkregistry   -v /mnt/registry:/var/lib/registry   registry:2
